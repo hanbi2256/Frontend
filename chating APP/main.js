5de197ea43c94716d1740fc21cbcd47702c282a7
hanbi <gksql2256@dsm.hs.kr>
2022-07-22 16:03:29 +0900

    chatting APP

---
+++ chating APP/main.js
@@ -0,0 +1,72 @@
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+
+const path = require('path');
+const net = require('net');
+const os = require('os');
+const readline = require('readline');
+const settings = require('./settings');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+    // win.webContents.openDevTools();
+
+    // renderer 프로세서 -> main 프로세서
+    ipcMain.on('login', (event, nickname) => {
+        let socket = net.createConnection(settings.server.port, settings.server.ip, () => {
+            socket.write(JSON.stringify({ action: 'login', msg: nickname }) + os.EOL);
+        });
+
+        const r1 = readline.createInterface({ input: socket });
+
+        r1.on('line', data => {
+            data = JSON.parse(data.toString());
+            switch (data.page) {
+                case 'login':
+                    if (data.msg == 'login success') {
+                        win.loadFile('chat.html');
+                    } else {
+                        win.webContents.send('loginMsg', data);
+                    }
+                    break;
+                case 'chat':
+                    win.webContents.send('chatMsg', data)
+
+            }
+        });
+
+        socket.on('close', () => {
+            dialog.showErrorBox('에러', '서버와 연결이 끊겼습니다.');
+            win.loadFile('index.html');
+        });
+
+        socket.on('error', () => { });
+
+        ipcMain.on('sendMsg', (event, msg) => {
+            socket.write(JSON.stringify({ action: 'msg', msg }) + os.EOL);
+        });
+    });
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
