ca9db2dacef37bfa4b32bfa5ce1c9b50ecfb5173
hanbi <gksql2256@dsm.hs.kr>
2022-07-31 22:59:30 +0900

    (js)일기장

--- made/일기장/일기장2.js
+++ made/일기장/일기장2.js
@@ -5,19 +5,24 @@ let toDoList = document.getElementById('toDoList');
 textBox.addEventListener("focus",function(){textBox.value=""});
 
 
-saveButton.addEventListener('click',save);
 
-function save()
-{
-    if(!textBox.value)
+saveButton.addEventListener('click', function(){    // 버튼에 클릭 이벤트가 발생하면
+    var list = document.createElement('div');  // html 'li' 태그 만들기
+   
+    if (!textBox.value)            // 할 일 입력창에 내용이 입력되지 않으면 alert 발생
+        alert('내용을 입력해 주세요!');
+    else
     {
-        alert("일기를 입력해주세요");
+        list.innerText = textBox.value;  // <li>입력된 할 일</li>
+        toDoList.appendChild(list);       // 할 일 리스트창에 자식으로 붙이기
+        textBox.value= "";               // 할 일 입력창 초기화
     }
-}
-save();
-'use strict';
 
-let itemList = [];
-let inputButton = document.querySelector(".input__button");
-inputButton.addEventListener("click", addItem);
+    list.addEventListener('click', function(){      // 만들어진 list에 클릭 이벤트가 발생하면 줄 긋기
+        list.style.textDecoration = "line-through";
+    })
+    list.addEventListener('dblclick', function(){   // list에 더블클릭 이벤트가 발생하면 할 일 리스트창에서 지우기
+        toDoList.removeChild(list);
+    })
+})
 
