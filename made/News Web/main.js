dfeebea4f6bf41386938f96e07f87f25e9427c11
hanbi <gksql2256@dsm.hs.kr>
2022-08-05 16:58:26 +0900

    (js)News web

--- made/News Web/main.js
+++ made/News Web/main.js
@@ -1,11 +1,21 @@
 
+let news = [];
 
-const getLatestNews=async()=>
-{
-    let url=new URL(` https://api.newscatcherapi.com/v2/latest_headlines?countries=kr&topic=business&page_size=10`);
-    let header=new Headers({'x-api-key':'kM52f3-AO9LEbk9bwl6T1ztr6DSM19qnxZzZcCwJXoM'});
-    let respones = await fetch(url,{headers:header});//ajx
-    let data=respones.json()
-    console.log(respones)
+const getLatestNews = async () => {
+    let url = new URL(` https://api.newscatcherapi.com/v2/latest_headlines?countries=kr&topic=business&page_size=10`);
+    let header = new Headers({ 'x-api-key': 'kM52f3-AO9LEbk9bwl6T1ztr6DSM19qnxZzZcCwJXoM' });
+    let respones = await fetch(url, { headers: header });//ajx
+    let data = await respones.json();
+    news = data.articles;
+    console.log(news);
+
+    render()
+
 };
 getLatestNews();
+
+const render = () => {
+    let newsHTML = ''
+
+    document.getElementById("").innerHTML=newsHTML
+}
