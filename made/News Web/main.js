6150421a2c5c9d893b1664aa32fadcbfeab7ceb9
hanbi <gksql2256@dsm.hs.kr>
2022-08-05 16:27:57 +0900

    (js)News web

--- made/News Web/main.js
+++ made/News Web/main.js
@@ -1,9 +1,11 @@
 
 
-const getLatestNews=()=>
+const getLatestNews=async()=>
 {
     let url=new URL(` https://api.newscatcherapi.com/v2/latest_headlines?countries=kr&topic=business&page_size=10`);
     let header=new Headers({'x-api-key':'kM52f3-AO9LEbk9bwl6T1ztr6DSM19qnxZzZcCwJXoM'});
-    let respones = fetch(url,{headers:header});
-}
+    let respones = await fetch(url,{headers:header});//ajx
+    let data=respones.json()
+    console.log(respones)
+};
 getLatestNews();
