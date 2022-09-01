dd6bc5f71865afc28810f026fb2aec9f163f4711
hanbi <gksql2256@dsm.hs.kr>
2022-09-01 20:01:25 +0900

    NT

--- Project/News Web/main.js
+++ Project/News Web/main.js
@@ -2,24 +2,21 @@
 let news = [];
 let menus = document.querySelectorAll(".menus button");
 
-
 menus.forEach(menu => menu.addEventListener("click", (event) => getNewsByTopic(event)));
 
 let searchButton = document.getElementById("search-button");
 let url;
 
-const  getNews=async()=>
-{
-    try{
+const getNews = async () => {
+    try {
         let header = new Headers({ 'x-api-key': 'kM52f3-AO9LEbk9bwl6T1ztr6DSM19qnxZzZcCwJXoM' });
         let respones = await fetch(url, { headers: header });
         let data = await respones.json();
         news = data.articles
         render();
     }
-    catch(error)
-    {
-        console.log("잡힌 에러는",error.messege);
+    catch (error) {
+        console.log("잡힌 에러는", error.messege);
     }
 }
 
@@ -29,13 +26,13 @@ const getLatestNews = async () => {
 
 };
 
-const getNewsByTopic = async (event) => {
+const getNewsByTopic = async (event) => {//토픽
     let topic = event.target.textContent.toLowerCase()
     url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10topic=${topic}`);
     getNews();
 };
 
-const getNewsByKeyword = async () => {
+const getNewsByKeyword = async () => {//키워드 검색
     let keyword = document.getElementById("search-input").value;
     url = new URL(`https://api.newscatcherapi.com/v2/search?q=${keyword}&page_size=10`);
     getNews();
@@ -58,12 +55,11 @@ const render = () => {
               ${item.rights} * ${item.publised_date}
             </div>
         </div>
-
-    </div>`
+</div>`
     }).join("");
-
     document.getElementById("news-board").innerHTML = newsHTML
 }
+
 searchButton.addEventListener("click", getNewsByKeyword);
 getLatestNews();
 
