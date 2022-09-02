126dc02703e2da0f22635727282829c233100722
hanbi <gksql2256@dsm.hs.kr>
2022-09-02 19:27:14 +0900

    ♻️ :: 코드 리펙토링

--- Project/News Web/main.js
+++ Project/News Web/main.js
@@ -1,13 +1,13 @@
 
 let news = [];
-let menus = document.querySelectorAll(".menus button");
+let menus = document.querySelectorAll(".menus button"); //메뉴에 있눈 버튼을 다 가지고 온다
 
 menus.forEach(menu => menu.addEventListener("click", (event) => getNewsByTopic(event)));
 
-let searchButton = document.getElementById("search-button");
+let searchButton = document.getElementById("search-button"); 
 let url;
 
-const getNews = async () => {
+const getNews = async () => { //awit은 async랑 짝짝꿍
     try {
         let header = new Headers({ 'x-api-key': 'kM52f3-AO9LEbk9bwl6T1ztr6DSM19qnxZzZcCwJXoM' });
         let respones = await fetch(url, { headers: header });
@@ -21,20 +21,20 @@ const getNews = async () => {
 }
 
 const getLatestNews = async () => {
-    url = new URL(` https://api.newscatcherapi.com/v2/latest_headlines?countries=kr&topic=business&page_size=10`);
+    url = new URL(` https://api.newscatcherapi.com/v2/latest_headlines?countries=kr&topic=business&page_size=100`);
     getNews();
 
 };
 
 const getNewsByTopic = async (event) => {//토픽
     let topic = event.target.textContent.toLowerCase()
-    url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10topic=${topic}`);
+    url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=100topic=${topic}`);
     getNews();
 };
 
 const getNewsByKeyword = async () => {//키워드 검색
     let keyword = document.getElementById("search-input").value;
-    url = new URL(`https://api.newscatcherapi.com/v2/search?q=${keyword}&page_size=10`);
+    url = new URL(`https://api.newscatcherapi.com/v2/search?q=${keyword}&page_size=100`);
     getNews();
 }
 
@@ -60,7 +60,7 @@ const render = () => {//보여지는 뉴스
     document.getElementById("news-board").innerHTML = newsHTML
 }
 
-searchButton.addEventListener("click", getNewsByKeyword);//searchButton을 눌렀을때 키워드 getNewsByKeyword부르기
+searchButton.addEventListener("click", getNewsByKeyword);//searchButton을 눌렀을때  getNewsByKeyword부르기
 
 getLatestNews();
 
